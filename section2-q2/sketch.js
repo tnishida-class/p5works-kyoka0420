65165991d6929cc47ba440f82daa4cbde6dbd240
kyoka0420 <72727405+kyoka0420@users.noreply.github.com>
2020-10-23 00:10:52 +0900

    checker

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,6 +6,19 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+    if (!(i % 2 == j % 2)){
+      fill(128)
+      rect(size * i,size * j,size,size)
+    }
+    if (!(i % 2 == j % 2) && j < 3){
+      fill(255,0,0)
+      ellipse(i * size + size/2,j * size +size/2, size-2)
+    }
+    else if(!(i % 2 == j % 2) && j > 4 ){
+      fill(0)
+      ellipse(i * size + size / 2,j * size + size / 2,size-2)
+    }
+
     }
   }
 }
