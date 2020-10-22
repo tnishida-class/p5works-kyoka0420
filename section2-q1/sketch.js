9797419a93c3e4799123d22223ce3525d3074862
kyoka0420 <72727405+kyoka0420@users.noreply.github.com>
2020-10-23 00:10:05 +0900

    kotesirabe

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,15 @@
 // 小手調べ
 function setup(){
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    noFill();
+    if (i<5){
+    stroke(0,0,255)
+    }
+    else{
+    stroke(255,0,0)
+    }
+let s = (i+1) * 10
+   ellipse(50,50,s);
+
   }
 }
