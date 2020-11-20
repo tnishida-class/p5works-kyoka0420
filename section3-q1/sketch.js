20eafb7b1df3caf8f75f10c1b5904efa7c7703b6
kyoka0420 <72727405+kyoka0420@users.noreply.github.com>
2020-11-20 20:58:15 +0900

    update2回目3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,17 +4,23 @@
 function setup(){
   createCanvas(400, 400);
   background(255)
-  balloon("I love keyakizaka46");
+  let a = color(0, 255, 127)
+  let b = color(0)
+  balloon("レポート多すぎ", 50, 60, a, b);
 }
 
-function balloon(t){
+function balloon(t, x, y, a, b){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
-  let p = 2
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
-  text(t, p, h + p);
-  fill(0);
-  triangle(w + p * 2, h + p * 2, w + p * 2 - 10, h + p * 2, w + p * 2 - 20, h + p * 2 + 50 )
+  let p = 4
+  fill(a);
+  noStroke();
+  rect(x, y, w + p * 2, h + p * 2);
+  fill(a);
+  noStroke();
+  fill(a)
+  triangle(x + p + w * 2 / 5, y + h + p * 2, x + p + w * 3 / 5, y + h + p * 2, x + p + w / 2, y + (h + p * 2) * 2  )
+  fill(b)
+  text(t, x + p, y + h + p);
+
 }
